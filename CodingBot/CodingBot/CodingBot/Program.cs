a93da53b6e729c849955e5d35a174cbc21516bc6
Samuil <35292702+sami119@users.noreply.github.com>
2018-12-28 18:27:38 +0200

    Discord bot first try

---
+++ CodingBot/CodingBot/CodingBot/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace CodingBot
+{
+    class Program
+    {
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+
+        static void Main(string[] args) 
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+
+            Client.MessageReceived += Client_MessageRecieved;
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            Client.Ready += Client_Ready;
+            Client.Log += Client_Log;
+
+            string Token = "";
+            using (var Stream = new FileStream((Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Replace(@"bin\Debug\netcoreapp2.0", @"Data\Token.txt"), FileMode.Open, FileAccess.Read))
+            using (var ReadToken = new StreamReader(Stream))
+            {
+                Token = ReadToken.ReadToEnd();
+            }
+            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Log(LogMessage Message)
+        {
+            Console.WriteLine($"{DateTime.Now} at {Message.Source}] {Message.Message}");
+        }
+
+        private async Task Client_Ready()
+        {
+            await Client.SetGameAsync("Looking for t {message}", "https://www.google.com/", StreamType.NotStreaming);
+        }
+
+        private async Task Client_MessageRecieved(SocketMessage arg)
+        {
+            //Configure the commands
+        }
+    }
+}
